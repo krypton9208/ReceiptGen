983c0ed6673cf9e9af504820ef27a984143dd712
krypton9208 <krypton9208@gmail.com>
2015-09-23 22:59:46 +0200

    Prace nad paragonami...

--- ReceiptGen/MainWindow.xaml.cs
+++ ReceiptGen/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using ImageMagick;
+using Color = System.Drawing.Color;
 
 namespace ReceiptGen
 {
@@ -23,6 +12,28 @@ namespace ReceiptGen
         public MainWindow()
         {
             InitializeComponent();
+            //MagickNET.Initialize(@"D:\");
+            MagickNET.SetTempDirectory(@"D:\Dokumenty\GitHub\ReceiptGen\ReceiptGen\bin\Debug\");
+            Paragon par = new Paragon(72);
+            par.DodajTytuł("Firma Handlowa \"BO-HEN\" Sp. Jawna", "Bożena i Henryk Zimnol", "43-520 Chybie",
+                "Mnich, ul. Kopernika 18", "NIP: 548-23-38-041", "19-09-2015                    W047160");  
+            Drukuj(par.GetTytul);
+        }
+
+        private void Drukuj(string text)
+        {
+            var img = new MagickImage("image.jpg");
+            using (var imgText = new MagickImage())
+            {
+                imgText.FontPointsize = 36;
+                imgText.BackgroundColor = new MagickColor(Color.White);
+                imgText.FillColor = new MagickColor(Color.Black);
+                imgText.AntiAlias = true;
+                imgText.FontFamily = "Fake Receipt";
+                imgText.Read("label:" + text);
+                img.Composite(imgText, Gravity.West);
+            }
+            image.Source = img.ToBitmapSource();
         }
     }
 }
---
+++ ReceiptGen/Paragon.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ReceiptGen
+{
+    public enum VAT {A, B } 
+    public class Zakupy
+    {
+        private double ilosc { get; set; }
+        private string Nazwa { get; set; }
+        private double Cena { get; set; }
+        private VAT Podatek { get; set; }
+        public Zakupy(int il, string name, double cena, VAT pod)
+        {
+            ilosc = il;
+            Nazwa = name;
+            Cena = cena;
+            Podatek = pod;
+        }
+
+        public double GetCena
+        {
+            get { return Cena; }
+        }
+    }
+    public class Paragon
+    {
+        private string Tytuł { get; set; }
+        public int szerokoscParagonu { get; set; }
+        private string WyrownajNaglowek(string text)
+        {
+            char znak  = '-';
+            string temp1 = "";
+            if (text.Length % 2 == 0)
+            {
+                int ilespacji = szerokoscParagonu - text.Length;
+                ilespacji -= text.Length/2;
+                ilespacji /= 2;
+                for (int i = 0; i < ilespacji; i++)
+                {
+                    temp1 += znak;
+                }
+                temp1 += text;
+                for (int i = 0; i < ilespacji; i++)
+                {
+                    temp1 += znak;
+                }
+            }
+            else
+            {
+                int ilespacji = szerokoscParagonu - text.Length;
+                ilespacji -= text.Length / 2;
+                ilespacji /= 2;
+                ilespacji++;
+                for (int i = 0; i < ilespacji; i++)
+                {
+                    temp1 += znak;
+                }
+                temp1 += text;
+                for (int i = 0; i < ilespacji - 1; i++)
+                {
+                    temp1 += znak;
+                }
+            }
+            return temp1;
+        }
+        private List<Zakupy> Lista { get; set; } 
+        public void DodajTytuł(string linia1, string linia2, string linia3, string linia4, string linia5,string linia6)
+        {
+            szerokoscParagonu = 72;
+            Tytuł = WyrownajNaglowek(linia1) + "\n" + WyrownajNaglowek(linia2) + "\n" + WyrownajNaglowek(linia3) +
+                "\n" + WyrownajNaglowek(linia4) + "\n" + WyrownajNaglowek(linia5) + "\n" + WyrownajNaglowek(linia6);
+        }
+
+        public Paragon(int szer)
+        {
+            Lista = new List<Zakupy>();
+            szerokoscParagonu = szer;
+        }
+
+        public double CenaParagonu
+        {
+            get
+            {
+                double temp = 0;
+                foreach (var item in Lista)
+                {
+                    temp += item.GetCena;
+                }
+                return temp;
+            }
+        }
+        public string GetTytul
+        {
+            get { return Tytuł; }
+        }
+
+    }
+}
